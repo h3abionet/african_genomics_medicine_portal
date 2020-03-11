c366a701faccc03c92158c877bdc7069728f5831
Ebo Turkson <mturkson23@gmail.com>
2020-03-11 11:32:45 +0000

    activate snp-diseases table in gene-centric view

--- agmp_app/static/js/search_details.js
+++ agmp_app/static/js/search_details.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // initialize data table for detail items
-  $('#detail-table, #snps-drugs, #star-alleles-drugs').DataTable({
+  $('#detail-table, #snps-drugs, #star-alleles-drugs, #snp-diseases').DataTable({
     dom: 'Bfrtip',
     buttons: [
       'copy', 'csv', 'excel', 'print'
