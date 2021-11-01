400683c972b9e8fab16c6315f383c00ba51e03e5
Perceval <mtrper001@myuct.ac.za>
2021-11-01 23:05:26 +0200

    created a new env

---
+++ env_pm/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/perceval/Documents/cbioworkspace/hackathon_new/african_genomics_medicine_portal/env_pm/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
