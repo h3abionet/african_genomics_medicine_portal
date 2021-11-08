0d9cb9841dbc78c5722f7326a0ba5785ef3a887a
codemeleon <anmol.kiran@gmail.com>
2021-11-08 12:16:55 +0200

    Bug fixes and updated requirements

--- agmp_app/management/commands/data_test.py
+++ agmp_app/management/commands/data_test.py
@@ -21,39 +21,38 @@ def genes_table_test(gene_file):
             usecols=["gene_id", "chromosome",
                      "gene_name", "uniprot_ac", "function"],
         )
-        gene_id_dup = data.groupby("gene_id").size().reset_index()
-        gene_id_dup = gene_id_dup[gene_id_dup[0] > 1]
-        if len(gene_id_dup):
-            dup_genes = ", ".join(gene_id_dup["gene_id"].values)
-            exit(f"{dup_genes} have duplicated values. exiting . . . .")
-
-        gene_name_dup = data.groupby("gene_name").size().reset_index()
-        gene_name_dup = gene_name_dup[gene_name_dup[0] > 1]
-        if len(gene_name_dup):
-            dup_genes = ", ".join(gene_name_dup["gene_name"].values)
-            exit(f"{dup_genes} have duplicated values. exiting . . . .")
-
-        # NOTE: Check the existance in the data base itself
-        for _, row in data.iterrows():
-            qry = Q(gene_id__exact=row["gene_id"]) | Q(
-                gene_name__exact=row["gene_name"]
-            )
-            genes_already_in_repo = read_frame(Gene.objects.filter(qry))
-            genes_already_in_repo = genes_already_in_repo[
-                ~(
-                    (genes_already_in_repo["gene_id"] == row["gene_id"])
-                    & (genes_already_in_repo["gene_name"] == row["gene_name"])
-                )
-            ]
-            if len(genes_already_in_repo):
-                print(
-                    f"Please check for [{row['gene_id']}, {row['gene_name']}]. They have have addition exiting values in repo table"
-                )
-
     except:
         exit(
             f"One or more columns among gene_id, chromosome, gene_name, uniprot_ac, function are missing {gene_file}"
         )
+
+    gene_id_dup = data.groupby("gene_id").size().reset_index()
+    gene_id_dup = gene_id_dup[gene_id_dup[0] > 1]
+    if len(gene_id_dup):
+        dup_genes = ", ".join(gene_id_dup["gene_id"].values)
+        print(f"{dup_genes} have duplicated values. exiting . . . .")
+
+    gene_name_dup = data.groupby("gene_name").size().reset_index()
+    gene_name_dup = gene_name_dup[gene_name_dup[0] > 1]
+    if len(gene_name_dup):
+        dup_genes = ", ".join(gene_name_dup["gene_name"].values)
+        print(f"{dup_genes} have duplicated values. exiting . . . .")
+
+    # NOTE: Check the existance in the data base itself
+    for _, row in data.iterrows():
+        qry = Q(gene_id__exact=row["gene_id"]) | Q(
+            gene_name__exact=row["gene_name"])
+        genes_already_in_repo = read_frame(Gene.objects.filter(qry))
+        genes_already_in_repo = genes_already_in_repo[
+            ~(
+                (genes_already_in_repo["gene_id"] == row["gene_id"])
+                & (genes_already_in_repo["gene_name"] == row["gene_name"])
+            )
+        ]
+        if len(genes_already_in_repo):
+            print(
+                f"Please check for [{row['gene_id']}, {row['gene_name']}]. They have have addition exiting values in repo table"
+            )
 
 
 def drugs_table_test(drug_file):
@@ -75,50 +74,51 @@ def drugs_table_test(drug_file):
                 "iupac_name",
             ],
         )
-        drug_id_dup = data.groupby("drug_id").size().reset_index()
-        drug_id_dup = drug_id_dup[drug_id_dup[0] > 1]
-        if len(drug_id_dup):
-            dup_drugs = ", ".join(drug_id_dup["drug_id"].values)
-            exit(f"{dup_drugs} have duplicated values. exiting . . . .")
-
-        drug_name_dup = data.groupby("drug_name").size().reset_index()
-        drug_name_dup = drug_name_dup[drug_name_dup[0] > 1]
-        if len(drug_name_dup):
-            dup_drugs = ", ".join(drug_name_dup["drug_name"].values)
-            exit(f"{dup_drugs} have duplicated values. exiting . . . .")
-
-        drug_id_dup = data.groupby("drug_id").size().reset_index()
-        drug_id_dup = drug_id_dup[drug_id_dup[0] > 1]
-        if len(drug_id_dup):
-            dup_drugs = ", ".join(drug_id_dup["drug_id"].values)
-            exit(f"{dup_drugs} have duplicated values. exiting . . . .")
-
-        # TODO: Add iupac_name in check list
-
-        # NOTE: Check the existance in the data base itself
-        for _, row in data.iterrows():
-            qry = (
-                Q(drug_id__exact=row["drug_id"])
-                | Q(drug_name__exact=row["drug_name"])
-                | Q(drug_bank_id__exact=row["drug_bank_id"])
-            )
-            drugs_already_in_repo = read_frame(Drug.objects.filter(qry))
-            drugs_already_in_repo = drugs_already_in_repo[
-                ~(
-                    (drugs_already_in_repo["drug_id"] == row["drug_id"])
-                    & (drugs_already_in_repo["drug_name"] == row["drug_name"])
-                    & (drugs_already_in_repo["drug_bank_id"] == row["drug_bank_id"])
-                )
-            ]
-            if len(drugs_already_in_repo):
-                print(
-                    f"Please check for [{row['drug_id']}, {row['drug_name']}, {row['drug_bank_id']}]. They have have addition exiting values in repo table"
-                )
-
     except:
         exit(
             f"One or more columns among drug_id, drug_name, drug_bank_id, state, indication, iupac_name are missing {drug_file}"
         )
+
+    drug_id_dup = data.groupby("drug_id").size().reset_index()
+    drug_id_dup = drug_id_dup[drug_id_dup[0] > 1]
+    if len(drug_id_dup):
+        dup_drugs = ", ".join(drug_id_dup["drug_id"].values)
+        print(f"{dup_drugs} have duplicated values. exiting . . . .")
+
+    drug_name_dup = data.groupby("drug_name").size().reset_index()
+    drug_name_dup = drug_name_dup[drug_name_dup[0] > 1]
+    if len(drug_name_dup):
+        dup_drugs = ", ".join(drug_name_dup["drug_name"].values)
+        # print(dup_drugs)
+        print(f"{dup_drugs} have duplicated values.")
+
+    drug_id_dup = data.groupby("drug_id").size().reset_index()
+    drug_id_dup = drug_id_dup[drug_id_dup[0] > 1]
+    if len(drug_id_dup):
+        dup_drugs = ", ".join(drug_id_dup["drug_id"].values)
+        print(f"{dup_drugs} have duplicated values. exiting . . . .")
+
+    # TODO: Add iupac_name in check list
+
+    # NOTE: Check the existance in the data base itself
+    for _, row in data.iterrows():
+        qry = (
+            Q(drug_id__exact=row["drug_id"])
+            | Q(drug_name__exact=row["drug_name"])
+            | Q(drug_bank_id__exact=row["drug_bank_id"])
+        )
+        drugs_already_in_repo = read_frame(Drug.objects.filter(qry))
+        drugs_already_in_repo = drugs_already_in_repo[
+            ~(
+                (drugs_already_in_repo["drug_id"] == row["drug_id"])
+                & (drugs_already_in_repo["drug_name"] == row["drug_name"])
+                & (drugs_already_in_repo["drug_bank_id"] == row["drug_bank_id"])
+            )
+        ]
+        if len(drugs_already_in_repo):
+            print(
+                f"Please check for [{row['drug_id']}, {row['drug_name']}, {row['drug_bank_id']}]. They have have addition exiting values in repo table"
+            )
 
 
 class Command(BaseCommand):
@@ -140,13 +140,13 @@ class Command(BaseCommand):
             type=str,
             help="csv file containing the drugs",
         )
-        # parser.add_argument(
-        # "--variants",
-        # action="store",
-        # dest="variants_file",
-        # type=str,
-        # help="csv file containing the variants",
-        # )
+        parser.add_argument(
+            "--variants",
+            action="store",
+            dest="variants_file",
+            type=str,
+            help="csv file containing the variants",
+        )
         # parser.add_argument(
         # "--keep",
         # action="store_true",
