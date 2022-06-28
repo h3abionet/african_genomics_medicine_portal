1cde6ef949b1c715ad6eeeeb4d76e7baed4a29f9
Perceval <mtrper001@myuct.ac.za>
2022-06-28 12:22:09 +0200

    changed header images, added folium map and graphs

--- agmp_app/models.py
+++ agmp_app/models.py
@@ -1,70 +1,91 @@
 from django.db import models
 from decimal import Decimal
+import geocoder
+
 
 class Index(models.Model):
-    id=models.AutoField(primary_key=True)
-    recname=models.CharField(max_length=250)
-    keywords=models.CharField(default="NA",max_length=250)
+    id = models.AutoField(primary_key=True)
+    recname = models.CharField(max_length=250)
+    keywords = models.CharField(default="NA", max_length=250)
+
 
 class drug(models.Model):
-    id=models.CharField(max_length=50, primary_key=True, default="DRUG0")
-    drug_name=models.CharField(max_length=250)
-    drug_bank_id=models.CharField(max_length=50, null=True)
-    state=models.CharField(max_length=250, null=True)
-    indication=models.TextField()
-    iupac_name=models.TextField()
+    id = models.CharField(max_length=50, primary_key=True, default="DRUG0")
+    drug_name = models.CharField(max_length=250)
+    drug_bank_id = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=250, null=True)
+    indication = models.TextField()
+    iupac_name = models.TextField()
+
 
 class disease(models.Model):
-    id=models.CharField(max_length=50, primary_key=True, default="DIS0")
-    disease_name=models.CharField(max_length=250)
+    id = models.CharField(max_length=50, primary_key=True, default="DIS0")
+    disease_name = models.CharField(max_length=250)
+
 
 class study(models.Model):
-    id=models.CharField(max_length=50, primary_key=True)
-    reference_id=models.CharField(max_length=50)
-    type=models.CharField(max_length=250)
-    year=models.CharField(max_length=10, default="NA")
-    title=models.CharField(max_length=250)
+    id = models.CharField(max_length=50, primary_key=True)
+    reference_id = models.CharField(max_length=50)
+    type = models.CharField(max_length=250)
+    year = models.CharField(max_length=10, default="NA")
+    title = models.CharField(max_length=250)
+
 
 class pharmacogenes(models.Model):
-    id=models.CharField(max_length=50, default="GENE0", primary_key=True)
-    chromosome_patch=models.CharField(default="NA",max_length=50)
-    gene_name=models.CharField(max_length=50, default="NA")
-    uniprot_id=models.CharField(max_length=50, null=True)
-    function=models.TextField(default="NA",max_length=250, null=True)
+    id = models.CharField(max_length=50, default="GENE0", primary_key=True)
+    chromosome_patch = models.CharField(default="NA", max_length=50)
+    gene_name = models.CharField(max_length=50, default="NA")
+    uniprot_id = models.CharField(max_length=50, null=True)
+    function = models.TextField(default="NA", max_length=250, null=True)
+
 
 class snp(models.Model):
-    id=models.AutoField(primary_key=True)
-    snp_id=models.CharField(max_length=50, default='NA')
-    rs_id=models.CharField(max_length=50, default="RS0")
-    gene=models.ForeignKey(pharmacogenes, on_delete=models.CASCADE, default="GENE0")
-    drug=models.ForeignKey(drug, on_delete=models.CASCADE, default="DRUG0")
-    disease=models.ForeignKey(disease, on_delete=models.CASCADE, default="DIS0")
-    allele=models.CharField(max_length=50)
-    association_with=models.TextField(default="NA")
-    reference=models.ForeignKey(study, on_delete=models.CASCADE)
-    p_value=models.CharField(max_length=10, null=True)
-    source=models.CharField(max_length=50, null=True)
-    id_in_source=models.CharField(max_length=50, null=True)
-    region=models.CharField(max_length=50, default="Undetermined")
-    country_of_participants=models.CharField(max_length=50, null=True)
-    latitude = models.DecimalField(max_digits=10,decimal_places=7,default=Decimal('0.0000000'))
-    longitude = models.DecimalField(max_digits=10,decimal_places=7,default=Decimal('0.0000000'))
+    id = models.AutoField(primary_key=True)
+    snp_id = models.CharField(max_length=50, default='NA')
+    rs_id = models.CharField(max_length=50, default="RS0")
+    gene = models.ForeignKey(
+        pharmacogenes, on_delete=models.CASCADE, default="GENE0")
+    drug = models.ForeignKey(drug, on_delete=models.CASCADE, default="DRUG0")
+    disease = models.ForeignKey(
+        disease, on_delete=models.CASCADE, default="DIS0")
+    allele = models.CharField(max_length=50)
+    association_with = models.TextField(default="NA")
+    reference = models.ForeignKey(study, on_delete=models.CASCADE)
+    p_value = models.CharField(max_length=10, null=True)
+    source = models.CharField(max_length=50, null=True)
+    id_in_source = models.CharField(max_length=50, null=True)
+    region = models.CharField(max_length=50, default="Undetermined")
+    country_of_participants = models.CharField(max_length=50, null=True)
+    latitude = models.DecimalField(
+        max_digits=10, decimal_places=7, default=Decimal('0.0000000'))
+    longitude = models.DecimalField(
+        max_digits=10, decimal_places=7, default=Decimal('0.0000000'))
+
+    def save(self, *args, **kwargs):
+        self.latitude = geocoder.osm(self.country_of_participants).lat
+        self.longitude = geocoder.osm(self.country_of_participants).lng
+        return super().save(*args, **kwargs)
+
 
 class star_allele(models.Model):
-    star_id=models.CharField(max_length=50, default='NA')
-    star_annotation=models.CharField(max_length=50)
-    gene=models.ForeignKey(pharmacogenes, on_delete=models.CASCADE, default="GENE0")
-    drug=models.ForeignKey(drug, on_delete=models.CASCADE, default="DRUG0")
-    allele=models.CharField(max_length=50)
-    phenotype=models.TextField(default="NA")
-    reference=models.ForeignKey(study, on_delete=models.CASCADE, null=True)
-    p_value=models.CharField(max_length=20, null=True)
-    source=models.CharField(max_length=50, null=True)
-    id_in_source=models.CharField(max_length=50, null=True)
-    region=models.CharField(max_length=50, default="Undetermined")
-    country_of_participants=models.CharField(max_length=50, null=True)
-    latitude = models.DecimalField(max_digits=10,decimal_places=7,default=Decimal('0.0000000'))
-    longitude = models.DecimalField(max_digits=10,decimal_places=7,default=Decimal('0.0000000'))
+    star_id = models.CharField(max_length=50, default='NA')
+    star_annotation = models.CharField(max_length=50)
+    gene = models.ForeignKey(
+        pharmacogenes, on_delete=models.CASCADE, default="GENE0")
+    drug = models.ForeignKey(drug, on_delete=models.CASCADE, default="DRUG0")
+    allele = models.CharField(max_length=50)
+    phenotype = models.TextField(default="NA")
+    reference = models.ForeignKey(study, on_delete=models.CASCADE, null=True)
+    p_value = models.CharField(max_length=20, null=True)
+    source = models.CharField(max_length=50, null=True)
+    id_in_source = models.CharField(max_length=50, null=True)
+    region = models.CharField(max_length=50, default="Undetermined")
+    country_of_participants = models.CharField(max_length=50, null=True)
+    latitude = models.DecimalField(
+        max_digits=10, decimal_places=7, default=Decimal('0.0000000'))
+    longitude = models.DecimalField(
+        max_digits=10, decimal_places=7, default=Decimal('0.0000000'))
+
 
 class CountryData(models.Model):
     country = models.CharField(max_length=100)
--- agmp_app/views.py
+++ agmp_app/views.py
@@ -1,3 +1,4 @@
+from urllib.parse import DefragResult
 from django.shortcuts import render, HttpResponse, redirect
 from django.http import FileResponse
 
@@ -7,18 +8,26 @@ from itertools import chain
 from .models import disease, pharmacogenes, drug, snp as SnpModel, star_allele, study
 from .forms import PostForm, CountryDataFrom
 import json
+import folium
+import geocoder
+from folium import plugins
 
 from agmp_app.models import *
+from django.db.models import Avg, Min, Max, Count, Q
+import pandas as pd
+
 
 # def index(request):
 #     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about.html')
 
+
 def search(request):
     form = PostForm(
-        initial = {
+        initial={
             'gene_name': '',
             # 'protein': '',
             'function': ''
@@ -26,8 +35,9 @@ def search(request):
     )
     return render(request, 'search.html', {"form": form})
 
+
 def _get_gene_page(query):
-    genes = pharmacogenes.objects.filter(id__exact= query).values()
+    genes = pharmacogenes.objects.filter(id__exact=query).values()
     details = dict()
     details['name'] = genes[0].get('gene_name')
     details['external_id'] = genes[0].get('uniprot_id')
@@ -66,6 +76,7 @@ INNER JOIN agmp_app_study on agmp_app_study.id = agmp_app_snp.reference_id \
 AND agmp_app_snp.gene_id = %s AND lower(agmp_app_snp.source) like 'disgenet%%';", [query]):
         details['snp_diseases'].append(p)
     return details
+
 
 def search_details(request, search_type, query_id):
     '''
@@ -189,11 +200,11 @@ INNER JOIN agmp_app_disease on agmp_app_disease.id = agmp_app_snp.disease_id
 AND agmp_app_disease.id =  %s;""", [query_id]):
             disease_list.append(p)
 
-    print (variant_drug)
-    print (gene_list)
-    print (gene_drug)
-    print (disease_list)
-    print (gene_details)
+    print(variant_drug)
+    print(gene_list)
+    print(gene_drug)
+    print(disease_list)
+    print(gene_details)
 
     return render(
         request, detail_view, {
@@ -205,8 +216,9 @@ AND agmp_app_disease.id =  %s;""", [query_id]):
             'gene_drug': gene_drug,
             'gene_details': gene_details,
             'diseases': disease_list,
-            }
-        )
+        }
+    )
+
 
 def _fetch_disease(diseases):
     '''
@@ -222,8 +234,9 @@ def _fetch_disease(diseases):
         disease_object['id'] = disease.get('id')
         disease_object['name'] = disease.get('disease_name')
         ret.append(disease_object)
-    print('DISEASE ',ret)
+    print('DISEASE ', ret)
     return ret
+
 
 def _fetch_drug(drugs):
     '''
@@ -250,19 +263,22 @@ def _fetch_drug(drugs):
     # print('DRUG ',ret)
     return ret
 
+
 def _fetch_variant(qs):
     '''
     :param list item_list: a list of item names to fetch from snp table
     :return dict
     '''
     ret = []
-    snps = SnpModel.objects.select_related('gene').filter(rs_id__icontains= qs).all()
+    snps = SnpModel.objects.select_related(
+        'gene').filter(rs_id__icontains=qs).all()
     print(snps)
     for snp in snps:
         # snp = snp.values()
         variant_object = dict()
         variant_object['key'] = 'vt'
-        variant_object['detail'] = ["<b>Chromosome</b> {0}".format(snp.gene.chromosome_patch), "<b>Gene: {0}</b>".format(snp.gene.gene_name)]
+        variant_object['detail'] = ["<b>Chromosome</b> {0}".format(
+            snp.gene.chromosome_patch), "<b>Gene: {0}</b>".format(snp.gene.gene_name)]
 
         variant_object['id'] = snp.snp_id
         variant_object['name'] = 'rs ID: {0}'.format(snp.rs_id)
@@ -279,6 +295,7 @@ def _fetch_variant(qs):
     # print('VARIANT ',ret)
     return ret
 
+
 def _fetch_gene(genes):
     '''
     :param list item_list: a list of item names to fetch from study table
@@ -289,7 +306,8 @@ def _fetch_gene(genes):
     for gene in genes:
         gene_object = dict()
         gene_object['key'] = 'ge'
-        gene_object['detail'] = ['Chromosome {0}'.format(gene.get('chromosome_patch'))]
+        gene_object['detail'] = [
+            'Chromosome {0}'.format(gene.get('chromosome_patch'))]
 
         gene_object['id'] = gene['id']
         gene_object['name'] = gene['gene_name']
@@ -299,6 +317,7 @@ def _fetch_gene(genes):
         ret.append(gene_object)
     # print('GENE ',ret)
     return ret
+
 
 def _fetch_data(model, item_list):
     '''
@@ -314,6 +333,7 @@ def _fetch_data(model, item_list):
     # TODO:
     pass
 
+
 def query(request, query_string, **kwargs):
     '''
     Get search query from ajax call
@@ -322,7 +342,7 @@ def query(request, query_string, **kwargs):
     # fetch the optional parameters from the request
     is_disease = int(request.GET.get('disease', 0))
     is_drug = int(request.GET.get('drug', 0))
-    is_variant =  int(request.GET.get('variant', 0))
+    is_variant = int(request.GET.get('variant', 0))
     is_gene = int(request.GET.get('gene', 0))
 
     pass_list = []
@@ -330,21 +350,27 @@ def query(request, query_string, **kwargs):
     if request.is_ajax():
         # TODO: there must be a better way to do this
         if is_disease:
-            disgenet_snps = SnpModel.objects.filter(source__icontains= 'DisGeNET')
-            disgenet_snps = disgenet_snps.filter(association_with__icontains=query_string)
+            disgenet_snps = SnpModel.objects.filter(
+                source__icontains='DisGeNET')
+            disgenet_snps = disgenet_snps.filter(
+                association_with__icontains=query_string)
             # pass_list += _fetch_disease(disgenet_snps.values())
-            pass_list += _fetch_disease(disease.objects.filter(disease_name__contains= query_string).values())
-            print (pass_list)
+            pass_list += _fetch_disease(disease.objects.filter(
+                disease_name__contains=query_string).values())
+            print(pass_list)
         if is_drug:
-            pass_list += _fetch_drug(drug.objects.filter(drug_name__contains= query_string).values())
+            pass_list += _fetch_drug(drug.objects.filter(
+                drug_name__contains=query_string).values())
         if is_variant:
             pass_list += _fetch_variant(query_string)
         if is_gene:
-            pass_list += _fetch_gene(pharmacogenes.objects.filter(gene_name__contains= query_string).values())
+            pass_list += _fetch_gene(pharmacogenes.objects.filter(
+                gene_name__contains=query_string).values())
 
     res = json.dumps(pass_list)
     mimetype = 'application/json'
     return HttpResponse(res, mimetype)
+
 
 def summary(request):
     '''
@@ -353,26 +379,103 @@ def summary(request):
     Returns the counts of records for the major models;
     drug, variant, disease, gene,
     '''
+    map_01 = folium.Map(
+        location=[4, 21], tiles='OpenStreetMap', control_scale=True, prefer_canvas=True, zoom_start=3)
+    data_list = snp.objects.values_list(
+        'latitude', 'longitude')
+
+    data = [[-33.918861, 18.423300, 3330], [55, 3, 100]]
+    all_points_on_map = pd.DataFrame(
+        list(snp.objects.all().values('latitude', 'longitude')))
+    # all_points_on_map2 = snp.objects.values_list('latitude', 'longitude')
+    # df = pd.DataFrame(all_points_on_map2)
+    # location_list = df.values.tolist()
+    # location_list_size = len(location_list)
+    # print(location_list_size)
+
+    # for point in range(0, location_list_size):
+    #     folium.Marker(location_list, [point]).add_to(map_01)
+
+    # plugins
+    plugins.HeatMap(data).add_to(map_01)
+    plugins.Fullscreen(position='topleft').add_to(map_01)
+    folium.Marker(location=[5.655576044317193, -
+                            0.1830446720123291]).add_to(map_01)
+
+    folium.raster_layers.TileLayer('Stamen Terrain').add_to(map_01)
+    folium.raster_layers.TileLayer('Stamen Toner').add_to(map_01)
+    folium.raster_layers.TileLayer('Stamen Watercolor').add_to(map_01)
+    folium.raster_layers.TileLayer('CartoDB Positron').add_to(map_01)
+    folium.raster_layers.TileLayer('CartoDB Dark_Matter').add_to(map_01)
+    folium.LayerControl().add_to(map_01)
+    map_01 = map_01._repr_html_()
+
     dgc = drug.objects.count()
     dsc = disease.objects.count()
     vtc = SnpModel.objects.count()
     gec = pharmacogenes.objects.count()
-    
+
+    # new gene graph Queries 12 June 2022
+    top_ten_pharmacogenes = pharmacogenes.objects.all().annotate(
+        num_of_publications=Count('snp')).order_by('-num_of_publications')[:10]
+
+    # new drug graph 26 June 2022
+    top_ten_drugs = drug.objects.all().annotate(
+        num_pubs=Count('snp')).order_by('-num_pubs')[:10]
+
+  # new graph variants 26 June 2022
+    top_ten_variants = star_allele.objects.all().annotate(
+        num_pubs=Count('star_id')).order_by('-num_pubs')[:10]
+
+    # top ten diseases 26 June 2022
+    top_ten_diseases = disease.objects.annotate(
+        num_of_pubs=Count('snp')).order_by('-num_of_pubs')[:10]
+    # top ten genes query2
+    # top_ten_genes = snp.objects.annotate(
+    #     num_of_genes=Count('gene_id')).order_by('-gene_id')[:10]
+
+    # //new graph queries
+    snp_data = snp.objects.all()
+    snp_data_by_country = snp.objects.all().values('country_of_participants')
+    # where country=USA
+    snp_usa = snp.objects.all().filter(
+        country_of_participants__iexact="USA")
+
+    snp_usa_number = snp.objects.all().filter(
+        country_of_participants__iexact="USA").count()
+    snp_distinct_country = snp.objects.all().values(
+        'country_of_participants', 'reference', 'id_in_source').filter(country_of_participants__icontains="USA")
+
+    # print("USA=", snp_usa)
+    # print("SSNP=", snp_data_by_country)
+
     counts = {}
     counts["Drugs"] = drug.objects.count()
     counts["Variants"] = snp.objects.count() + star_allele.objects.count()
     counts["Diseases"] = disease.objects.count()
     counts["Genes"] = pharmacogenes.objects.count()
     context = {
-        'drug_count': dgc, 
-        'disease_count': dsc, 
+        'data_list': data_list,
+        # 'all_points_on_map': all_points_on_map,
+        'map_01': map_01,
+        'top_ten_diseases': top_ten_diseases,
+        'top_ten_drugs': top_ten_drugs,
+        'top_ten_variants': top_ten_variants,
+        'top_ten_pharmacogenes': top_ten_pharmacogenes,
+        # 'top_ten_genes': top_ten_genes,
+        'snp_usa_number': snp_usa_number,
+        'snp_usa': snp_usa,
+        'snp_data': snp_data,
+        'drug_count': dgc,
+        'disease_count': dsc,
         'variant_count': vtc,
         'gene_count': gec,
         'count_keys': json.dumps(list(counts.keys())),
         'count_data': json.dumps(list(counts.values())),
-        'records': [{'LAT': 1.000, 'LON':-1.000, }]
-        }
+        'records': [{'LAT': 1.000, 'LON': -1.000, }]
+    }
     return render(request, 'summary.html', context)
+
 
 def country_summary(request):
     '''
@@ -383,48 +486,61 @@ def country_summary(request):
     for p in SnpModel.objects.raw("SELECT DISTINCT id, country_of_participants, latitude, longitude, region, snp_id, \
         COUNT(country_of_participants) AS cnt FROM agmp_app_snp WHERE (latitude<>'N/A' AND longitude<>'N/A') GROUP BY country_of_participants ORDER BY cnt;"):
         res.append({
+
             'country': p.country_of_participants,
             'region': p.region,
             'latitude': '{:,.7f}'.format(p.latitude),
             'longitude': '{0}'.format(p.longitude),
             'snp': p.snp_id,
             'count': p.cnt
-        })  
+
+        })
     # return render(request, 'summary.html', {'json_list': res})
 
     res = json.dumps(res)
     mimetype = 'application/json'
     return HttpResponse(res, mimetype)
 
+
 def resources(request):
     return render(request, 'resources.html')
+
 
 def outreach(request):
     return render(request, 'outreach.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
+
 
 def disclaimer(request):
     return render(request, 'disclaimer.html')
 
+
 def faqs(request):
     return render(request, 'faqs.html')
+
 
 def tools_pipelines(request):
     return render(request, 'tools_pipelines.html')
 
+
 def databases(request):
     return render(request, 'resources.html')
+
 
 def online_courses(request):
     return render(request, 'online_courses.html')
 
+
 def help(request):
     return render(request, 'help.html')
 
+
 def tutorial(request):
     return render(request, 'tutorial.html')
+
 
 def home(request):
     return render(request, 'home.html')
