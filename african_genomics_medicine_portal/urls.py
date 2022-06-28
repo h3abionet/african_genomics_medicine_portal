c18edec05bb46a7bab314b254481f47c42f215d3
Ayton Meintjes <aytonm@gmail.com>
2022-06-28 14:40:17 +0200

    non-default admin url

--- african_genomics_medicine_portal/urls.py
+++ african_genomics_medicine_portal/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^madiba/', admin.site.urls),
     path('', include('agmp_app.urls')),
 ]
