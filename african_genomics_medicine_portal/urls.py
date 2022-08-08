fc65cd9ee5c5e9d72078c7848c7c18976a345d36
Ayton Meintjes <aytonm@gmail.com>
2022-08-08 19:32:35 +0200

    admin url

--- african_genomics_medicine_portal/urls.py
+++ african_genomics_medicine_portal/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path('', include('agmp_app.urls')),
 ]
