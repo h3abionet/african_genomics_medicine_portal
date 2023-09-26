444dbec0ee35d9b7e1f0e2260198ecd06bc0ac6b
Perceval <mtrper001@myuct.ac.za>
2023-09-26 04:37:47 +0100

    added import script

--- agmp_app/admin.py
+++ agmp_app/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import pharmacogenes, drug, snp, star_allele, study, CountryData
+from .models import pharmacogenes, drug, snp, star_allele, study, CountryData, Variantagmp, Drugagmp, Geneagmp, VariantStudyagmp, Studyagmp, Phenotypeagmp
+
+class VariantagmpAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    # pass
+class DrugagmpAdmin(admin.ModelAdmin):
+    list_display = ['id','drug_id']
+    search_fields =['drug_id']
+
+class GeneagmpAdmin(admin.ModelAdmin):
+    list_display = ['id', 'gene_id']
+    search_fields =['gene_id']
+  
+
+class VariantStudyagmpAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    search_fields =['country_participant','notes']
+
+class StudyagmpAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
+class PhenotypeagmpAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
+###### site.register ######
+admin.site.register(Drugagmp, DrugagmpAdmin)
+admin.site.register(Variantagmp, VariantagmpAdmin)
+admin.site.register(Geneagmp, GeneagmpAdmin)
+admin.site.register(VariantStudyagmp, VariantStudyagmpAdmin)
+admin.site.register(Studyagmp, StudyagmpAdmin)
+admin.site.register(Phenotypeagmp, PhenotypeagmpAdmin)
 
 admin.site.site_header = 'AGMP admin'
 admin.site.site_title = 'AGMP admin'
