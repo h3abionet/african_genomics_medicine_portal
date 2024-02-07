733b72cec374e6443dca68aa93d3282864ff400f
Perceval <mtrper001@myuct.ac.za>
2024-02-07 12:58:22 +0000

    emptied static_cdn folder

--- static_cdn_old/js/search_details.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  // initialize data table for detail items
-  $('#detail-table, #snps-drugs, #star-alleles-drugs, #snp-diseases').DataTable({
-    dom: 'Bfrtip',
-    buttons: [
-      'copy', 'csv', 'excel', 'print'
-    ]    
-  });
-});
