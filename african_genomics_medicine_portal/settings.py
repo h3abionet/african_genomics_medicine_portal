ede89af369447f5ca6f986f8fce627e6a9efed42
Perceval <mtrper001@myuct.ac.za>
2025-04-16 10:13:49 +0100

    added a secret key config

--- african_genomics_medicine_portal/settings.py
+++ african_genomics_medicine_portal/settings.py
@@ -49,8 +49,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "config.SECRET_KEY"
 
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # as a failsafe if you don't have this flag in config switch on production
 DEBUG = False
